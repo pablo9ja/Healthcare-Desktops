eaae2e2358fa382d0a09b38a0d7ed46b8b2e0e04
Pablo9ja <pablocito.kef2@yahoo.com>
2024-07-19 12:20:03 +0100

    requiret

--- iapp.py
+++ iapp.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import os
-import warnings
-warnings.filterwarnings('ignore')
+#import os
+#import warnings
+#warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Healthcare!!!", page_icon=":bar_chart:", layout="wide")
 
